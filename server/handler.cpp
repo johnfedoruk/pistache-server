e5b275fbbda6c8830ba5392d067528313245549d
johnfedoruk <johnny@johnfedoruk.ca>
2018-02-18 19:19:49 -0600

    Using AuthToken headers to authenticate ServerHandler

--- server/handler.cpp
+++ server/handler.cpp
@@ -1,6 +1,28 @@
+#include <string>
 #include "handler.hpp"
+#include "headers/auth_token.hpp"
+
+static const std::string TOKEN = "f8fe3bc90778c9eb1e3a80ac5ac4b7b71e50eeee13a887878dc6a683972aa0dd";
 
 void ServerHandler::onRequest(const Pistache::Http::Request &request, Pistache::Http::ResponseWriter response)
 {
-    response.send(Pistache::Http::Code::Ok, "Hello world");
+    try
+    {
+        auto headers = request.headers();
+        auto token_header = headers.tryGet<AuthToken>();
+        if (token_header)
+        {
+            std::string token = token_header->token();
+            if (TOKEN.compare(token) == 0)
+            {
+                response.send(Pistache::Http::Code::Ok, "Hello, World");
+                return;
+            }
+        }
+        response.send(Pistache::Http::Code::Unauthorized, "Unauthorized");
+    }
+    catch (int e)
+    {
+        response.send(Pistache::Http::Code::Unauthorized, "Unauthorized");
+    }
 }
