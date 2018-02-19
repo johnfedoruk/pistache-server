b1142c1c25df4369f0b7df8fe230665c3a997850
johnfedoruk <johnny@johnfedoruk.ca>
2018-02-18 19:19:05 -0600

    Registering AuthToken headers

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 
 #include "server/handler.hpp"
+#include "server/headers/auth_token.hpp"
 
 #define CORES 4
 #define PORT 8080
@@ -13,6 +14,7 @@ int main(int argc, char *argv[])
     const unsigned int cores = CORES;
     const unsigned int port = PORT;
     std::cout << "Starting server on port " << port << " with " << cores << " cores" << std::endl;
+    Pistache::Http::Header::Registry::registerHeader<AuthToken>();
     Pistache::Address addr(Pistache::Ipv4::any(), Pistache::Port(port));
     auto opts = Pistache::Http::Endpoint::options().threads(cores);
     Pistache::Http::Endpoint server(addr);
